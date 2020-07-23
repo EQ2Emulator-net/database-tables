1b610f6ced72c28915cd69069fff33bf1c404bea
Cynnar <jacob0305@gmail.com>
2020-07-23 10:40:10 +0000

    Automatic Content Update

--- lootdrop.sql
+++ lootdrop.sql
@@ -24,7 +24,7 @@ CREATE TABLE `lootdrop` (
   KEY `FK_lootdrop` (`item_id`),
   CONSTRAINT `FK_lootdrop` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_loottable` FOREIGN KEY (`loot_table_id`) REFERENCES `loottable` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=23091 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23179 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
