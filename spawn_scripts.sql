f88370c4d70df8f6ffc58b2282ab9c4bdcd0212d
Cynnar <jacob0305@gmail.com>
2020-08-19 10:40:14 +0000

    Automatic Content Update

--- spawn_scripts.sql
+++ spawn_scripts.sql
@@ -19,7 +19,7 @@ CREATE TABLE `spawn_scripts` (
   `spawn_location_id` int(10) unsigned NOT NULL DEFAULT 0,
   `lua_script` varchar(255) COLLATE latin1_general_ci NOT NULL DEFAULT ' ',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=144253 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=144257 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
