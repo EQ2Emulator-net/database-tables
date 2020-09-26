63a4f780e51ee68a8ead2de698efd5ef8499cbc3
Cynnar <jacob0305@gmail.com>
2020-09-26 10:40:07 +0000

    Nightly Updates

--- spell_tiers.sql
+++ spell_tiers.sql
@@ -47,7 +47,7 @@ CREATE TABLE `spell_tiers` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `SpellTierIDX` (`spell_id`,`tier`),
   CONSTRAINT `FK_spell_tiers` FOREIGN KEY (`spell_id`) REFERENCES `spells` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=61454 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=61462 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
