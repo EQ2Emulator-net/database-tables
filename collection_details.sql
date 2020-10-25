5ce410d25efbcd1aa386c962cb50fee86688bf05
Cynnar <jacob0305@gmail.com>
2020-10-25 10:40:09 +0000

    Nightly Updates

--- collection_details.sql
+++ collection_details.sql
@@ -20,7 +20,7 @@ CREATE TABLE `collection_details` (
   PRIMARY KEY (`id`),
   KEY `FK_collection_details` (`collection_id`),
   CONSTRAINT `FK_collection_details` FOREIGN KEY (`collection_id`) REFERENCES `collections` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3499 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3500 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
