4c258f9c414199d925a2ce1f53f147ed0188c7ce
Cynnar <jacob0305@gmail.com>
2020-11-28 10:40:09 +0000

    Nightly Updates

--- items.sql
+++ items.sql
@@ -89,7 +89,7 @@ CREATE TABLE `items` (
   UNIQUE KEY `ItemIDX` (`soe_item_id`),
   KEY `ItemTypeIDX` (`item_type`),
   KEY `ItemNameIDX` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=192188 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=192189 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
