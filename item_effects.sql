e33fd5a4b47c96159a6dd558bc5bd32a02accbff
Cynnar <jacob0305@gmail.com>
2021-01-23 10:40:22 +0000

    Nightly Updates

--- item_effects.sql
+++ item_effects.sql
@@ -21,7 +21,7 @@ CREATE TABLE `item_effects` (
   PRIMARY KEY (`id`),
   KEY `FK_item_effects` (`item_id`),
   CONSTRAINT `FK_item_effects` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=241840 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=241841 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
