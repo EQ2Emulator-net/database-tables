3759725a753efb7f8c83d1c495cd193e8a7bf7c3
Cynnar <jacob0305@gmail.com>
2021-01-31 10:40:16 +0000

    Nightly Updates

--- character_collections.sql
+++ character_collections.sql
@@ -23,7 +23,7 @@ CREATE TABLE `character_collections` (
   CONSTRAINT `FK_character_collections` FOREIGN KEY (`char_id`) REFERENCES `characters` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_charactercollections_charid` FOREIGN KEY (`char_id`) REFERENCES `characters` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_charactercollections_collectionid` FOREIGN KEY (`collection_id`) REFERENCES `collections` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=791 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=796 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
