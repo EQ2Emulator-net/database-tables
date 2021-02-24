30648fd893ebdc7d65899ddde7736aa93994c5ac
Cynnar <jacob0305@gmail.com>
2021-02-24 10:40:14 +0000

    Nightly Updates

--- character_spirit_shards.sql
+++ character_spirit_shards.sql
@@ -51,7 +51,7 @@ CREATE TABLE `character_spirit_shards` (
   `instanceid` int(10) unsigned NOT NULL DEFAULT 0,
   `charid` int(10) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
