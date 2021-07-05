9b2918168f2b7962661029e818f8cb0544ac1af9
Cynnar <jacob0305@gmail.com>
2021-07-05 10:40:19 +0000

    Nightly Updates

--- instances.sql
+++ instances.sql
@@ -18,7 +18,7 @@ CREATE TABLE `instances` (
   PRIMARY KEY (`id`),
   KEY `ZoneIDX` (`zone_id`),
   CONSTRAINT `FK_instances` FOREIGN KEY (`zone_id`) REFERENCES `zones` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=577 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=578 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
