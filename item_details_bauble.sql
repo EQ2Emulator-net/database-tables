1497d74f33a9cebadd0ee63d1d0aeb903e38bb85
Cynnar <jacob0305@gmail.com>
2021-12-08 10:40:17 +0000

    Nightly Updates

--- item_details_bauble.sql
+++ item_details_bauble.sql
@@ -29,7 +29,7 @@ CREATE TABLE `item_details_bauble` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `ItemBaubleIDX` (`item_id`),
   CONSTRAINT `FK_item_details_bauble` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=8413 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8414 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
