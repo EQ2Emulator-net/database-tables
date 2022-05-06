2993934774964ed8b471f5c1e7bc44e72194084c
Cynnar <jacob0305@gmail.com>
2022-05-06 10:40:15 +0000

    Nightly Updates

--- character_macros.sql
+++ character_macros.sql
@@ -22,7 +22,7 @@ CREATE TABLE `character_macros` (
   PRIMARY KEY (`id`),
   KEY `FK_character_macros` (`char_id`),
   CONSTRAINT `FK_character_macros` FOREIGN KEY (`char_id`) REFERENCES `characters` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1360 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1362 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
