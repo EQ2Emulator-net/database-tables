0fbf9c8ed8a3908e620453e618dc75642df4e02e
Cynnar <jacob0305@gmail.com>
2022-05-22 10:40:40 +0000

    Nightly Updates

--- item_details_armor.sql
+++ item_details_armor.sql
@@ -23,7 +23,7 @@ CREATE TABLE `item_details_armor` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `NewIndex` (`item_id`),
   CONSTRAINT `FK_item_details_armor` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=124394 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=124395 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
