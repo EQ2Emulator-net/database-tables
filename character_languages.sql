2b8798faf84b2ab366c46837b786d02417279f3d
Cynnar <jacob0305@gmail.com>
2022-05-31 10:40:27 +0000

    Nightly Updates

--- character_languages.sql
+++ character_languages.sql
@@ -19,7 +19,7 @@ CREATE TABLE `character_languages` (
   PRIMARY KEY (`id`),
   KEY `FK_character_languages` (`char_id`),
   CONSTRAINT `character_languages_ibfk_1` FOREIGN KEY (`char_id`) REFERENCES `characters` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- item_details_range.sql
+++ item_details_range.sql
@@ -30,7 +30,7 @@ CREATE TABLE `item_details_range` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `NewIndex` (`item_id`),
   CONSTRAINT `FK_item_details_range` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=7097 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7098 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
