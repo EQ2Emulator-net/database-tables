2803cd80fe86922b4f820b886e9965ff706dc829
Cynnar <jacob0305@gmail.com>
2022-06-10 10:40:13 +0000

    Nightly Updates

--- loot_global.sql
+++ loot_global.sql
@@ -23,7 +23,7 @@ CREATE TABLE `loot_global` (
   PRIMARY KEY (`id`),
   KEY `FKLootTable` (`loot_table`),
   CONSTRAINT `FKLootTable` FOREIGN KEY (`loot_table`) REFERENCES `loottable` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=269 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=271 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
